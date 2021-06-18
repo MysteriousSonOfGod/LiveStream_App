e4cf159e9469f95c8924962a9aad710564559193
Ingyu Yang <79055280+igoo-Y@users.noreply.github.com>
2021-06-18 16:25:50 +0900

    live stream

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,4 @@ from channels import views as channel_views
 
 app_name = "core"
 
-urlpatterns = [path("", channel_views.HomeView, name="home")]
+urlpatterns = [path("", channel_views.home, name="home")]
